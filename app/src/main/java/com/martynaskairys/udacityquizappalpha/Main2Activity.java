e99042df66812afd2257d8d737671cf0a9dc9bae
martynaskairys <martynaskairys@gmail.com>
2017-04-04 11:41:44 +0300

    changing code in order to get the right score

--- app/src/main/java/com/martynaskairys/udacityquizappalpha/Main2Activity.java
+++ app/src/main/java/com/martynaskairys/udacityquizappalpha/Main2Activity.java
@@ -1,18 +1,15 @@
 package com.martynaskairys.udacityquizappalpha;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static com.martynaskairys.udacityquizappalpha.R.id.score2;
-import static com.martynaskairys.udacityquizappalpha.R.id.showHome;
 
 public class Main2Activity extends AppCompatActivity {
-
 
 
     @Override
@@ -23,36 +20,32 @@ public class Main2Activity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            int score = extras.getInt("SCORE");
-            //The key argument here must match that used in the other activity
+            int numberOfQuestionsCorrect = extras.getInt("SCORE");
 
-            if(score>2)
+            if (numberOfQuestionsCorrect >= 2)
 
             {
 
                 Toast.makeText(this, R.string.quiz_completion_greeting, Toast.LENGTH_SHORT).show();
             }
+else {
+                Toast.makeText(this, R.string.quiz_failure_greeting, Toast.LENGTH_SHORT).show();
+            }
 
             TextView scoreView = (TextView) findViewById(score2);
-            scoreView.setText(String.valueOf(score));
+            scoreView.setText(String.valueOf(numberOfQuestionsCorrect));
         }
     }
 
 
-public void restart (View view){
-
-  /*  Intent i = getBaseContext().getPackageManager()
-            .getLaunchIntentForPackage( getBaseContext().getPackageName() );
-    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    startActivity(i);*/
-
-  Intent i = new Intent(Main2Activity.this, MainActivity.class);
-    startActivity(i);
-
-}
+    public void restart(View view) {
 
 
+        Intent i = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(i);
 
+
+    }
 
 
 }
--- app/src/main/java/com/martynaskairys/udacityquizappalpha/MainActivity.java
+++ app/src/main/java/com/martynaskairys/udacityquizappalpha/MainActivity.java
@@ -1,58 +1,114 @@
 package com.martynaskairys.udacityquizappalpha;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
 
+    final int Q1_ANSWER = R.id.question_1_third_logo;
+    final int Q2_ANSWER = R.id.question_2_second_logo;
+    final String Q4_ANSWER = "none";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button5);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                intent.putExtra("SCORE", score);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void addOne(View view) {
-
-        score = score + 1;
 
     }
 
-    public void addTwo(View view) {
+    public void onClick(View v) {
+        ArrayList<String> incorrectAnswersList = new ArrayList<>();
 
-        score = score + 2;
+        int numberOfQuestionsCorrect = 0;
+
+        if (checkQuestion1()) {
+            numberOfQuestionsCorrect++;
+        } else {
+            incorrectAnswersList.add("Question 1");
+        }
+
+        if (checkQuestion2()) {
+            numberOfQuestionsCorrect++;
+        } else {
+            incorrectAnswersList.add("Question 2");
+        }
+
+        if (checkQuestion3()) {
+            numberOfQuestionsCorrect++;
+        } else {
+            incorrectAnswersList.add("Question 3");
+        }
+
+        if (checkQuestion4()) {
+            numberOfQuestionsCorrect++;
+        } else {
+            incorrectAnswersList.add("Question 4");
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : incorrectAnswersList) {
+            sb.append(s);
+            sb.append("\n");
+        }
+
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra("SCORE", numberOfQuestionsCorrect);
+        startActivity(intent);
+
+        /*Context context = getApplicationContext();
+        CharSequence text = "You got " + numberOfQuestionsCorrect + "/4 answers right.\n\nRecheck the following:\n" + sb.toString();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();*/
+    }
+
+    private boolean checkQuestion1() {
+        RadioGroup rg = (RadioGroup) findViewById(R.id.question_1);
+
+        {
+            return (rg.getCheckedRadioButtonId() == Q1_ANSWER);
+        }
+
 
     }
 
-    public void addThree(View view) {
+    private boolean checkQuestion2() {
+        RadioGroup rg = (RadioGroup) findViewById(R.id.question_2);
 
-        score = score + 3;
-
-    }
-
-    public void addFour(View view) {
-
-        score = score + 1;
+        {
+            return (rg.getCheckedRadioButtonId() == Q2_ANSWER);
+        }
 
     }
 
+    private boolean checkQuestion3() {
+        CheckBox c1 = (CheckBox) findViewById(R.id.question_3_first_checkbox);
+        CheckBox c2 = (CheckBox) findViewById(R.id.question_3_second_checkbox);
+        CheckBox c3 = (CheckBox) findViewById(R.id.question_3_third_checkbox);
+        CheckBox c4 = (CheckBox) findViewById(R.id.question_3_fourth_checkbox);
+
+        {
+            return (c1.isChecked() && !c2.isChecked() && c3.isChecked() && !c4.isChecked());
+        }
+
+    }
+
+    private boolean checkQuestion4() {
+        EditText et = (EditText) findViewById(R.id.question_4_edit_text);
+
+        return et.getText().toString().equalsIgnoreCase(Q4_ANSWER);
+    }
 
 
 }
